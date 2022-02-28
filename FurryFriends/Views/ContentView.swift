2576162243899ee342a7c9b98e6fff792400d653
Leo Lu <llu@lcs.on.ca>
2022-02-28 17:40:08 -0500

    load a new dog image from the endpoint by pressing the button (one problem unsolved)

--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     // Address for main image
     // Starts as a transparent pixel – until an address for an animal's image is set
-    @State var currentImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
+    @State var currentImage: DogImage = DogImage(message: "", status: "")
     
     // MARK: Computed properties
     var body: some View {
@@ -21,13 +21,16 @@ struct ContentView: View {
         VStack {
             
             // Shows the main image
-            RemoteImageView(fromURL: currentImage)
+            RemoteImageView(fromURL: URL(string: currentImage.message)!)
             
             Image(systemName: "heart.circle")
                 .font(.largeTitle)
             
             Button(action: {
                 print("I was pressed")
+                Task {
+                    await loadNewImage()
+                }
             }, label: {
                 Text("Another one!")
             })
@@ -48,14 +51,8 @@ struct ContentView: View {
         }
         // Runs once when the app is opened
         .task {
-            
-            // Example images for each type of pet
-            let remoteDogImage = "https://images.dog.ceo/breeds/labrador/lab_young.JPG"
-            
-            // Replaces the transparent pixel image with an actual image of an animal
-            // Adjust according to your preference ☺️
-            currentImage = URL(string: remoteDogImage)!
-                        
+            await loadNewImage()
+            print("Have just attempted to load a new image.")
         }
         .navigationTitle("Furry Friends")
         .padding()
@@ -63,7 +60,19 @@ struct ContentView: View {
     }
     
     // MARK: Functions
-    
+    func loadNewImage() async {
+        let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        let urlSession = URLSession.shared
+        do {
+            let (data, _) = try await urlSession.data(for: request)
+            currentImage = try JSONDecoder().decode(DogImage.self, from: data)
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            print(error)
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
